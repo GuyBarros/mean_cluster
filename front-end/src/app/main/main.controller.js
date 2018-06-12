cc4092fda1fa0301af1d55a5d1391b681ba9e49f
Guy Barros <guy@hashicorp.com>
2018-06-13 00:03:55 +0100

    reading messages from mongo

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -3,9 +3,17 @@ export class MainController {
     'ngInject';
 
     this.$http = $http;
+    this.getMessages();
 
 }
  postMessage(){
    this.$http.post('http://localhost:5000/api/message', {msg: this.message })
+ }
+
+ getMessages(){
+   var vm = this;
+  this.$http.get('http://localhost:5000/api/message').then(function(result){
+    vm.messages = result.data;
+  })
  }
 }
